bbca924ab177cfefdd331b624635b31ac1262659
Lekret <lekret399@mail.ru>
2022-10-06 12:27:36 +0300

    recycled ids with queue

--- SimpleEcs/Internal/IdGenerator.cs
+++ SimpleEcs/Internal/IdGenerator.cs
@@ -4,23 +4,21 @@ namespace SimpleEcs.Internal
 {
     internal sealed class IdGenerator
     {
-        private readonly HashSet<int> _takenIds = new HashSet<int>();
+        private readonly Queue<int> _recycled = new Queue<int>();
         private int _currentId;
         internal int CurrentId => _currentId;
         
         internal int Next()
         {
-            while (_takenIds.Contains(_currentId))
-            {
-                _currentId++;
-            }
-            _takenIds.Add(_currentId);
+            if (_recycled.Count > 0)
+                return _recycled.Dequeue();
+            _currentId++;
             return _currentId;
         }
 
         internal void ReleaseId(int id)
         {
-            _takenIds.Remove(id);
+            _recycled.Enqueue(id);
         }
     }
 }
