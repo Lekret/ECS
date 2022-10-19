bbb774366b72f3a3bbc38c139ced88d0f357058c
Lekret <lekret399@mail.ru>
2022-10-19 03:47:10 +0300

    Systems

---
+++ SimpleEcs/ISystem.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleEcs
+{
+    public interface ISystem { }
+
+    public interface IInitSystem : ISystem
+    {
+        void Init();
+    }
+    
+    public interface IUpdateSystem : ISystem
+    {
+        void Update();
+    }
+    
+    public interface IFixedUpdateSystem : ISystem
+    {
+        void FixedUpdate();
+    }
+    
+    public interface ILateUpdateSystem : ISystem
+    {
+        void LateUpdate();
+    }
+
+    public interface IDestroySystem : ISystem
+    {
+        void Destroy();
+    }
+}
---
+++ SimpleEcs/Systems.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleEcs
+{
+	public class Systems : 
+		IInitSystem, 
+		IUpdateSystem, 
+		IFixedUpdateSystem,
+		ILateUpdateSystem, 
+		IDestroySystem
+	{
+		protected List<IInitSystem> InitSystems = new List<IInitSystem>();
+		protected List<IUpdateSystem> UpdateSystem = new List<IUpdateSystem>();
+		protected List<IFixedUpdateSystem> FixedUpdateSystems = new List<IFixedUpdateSystem>();
+		protected List<ILateUpdateSystem> LateUpdateSystems = new List<ILateUpdateSystem>();
+		protected List<IDestroySystem> DestroySystems = new List<IDestroySystem>();
+
+		public virtual Systems Add(ISystem system)
+		{
+			if (system is IInitSystem initSystem)
+			{
+				InitSystems.Add(initSystem);
+			}
+
+			if (system is IFixedUpdateSystem fixedUpdateSystem)
+			{
+				FixedUpdateSystems.Add(fixedUpdateSystem);
+			}
+
+			if (system is IUpdateSystem updateSystem)
+			{
+				UpdateSystem.Add(updateSystem);
+			}
+
+			if (system is ILateUpdateSystem lateUpdateSystem)
+			{
+				LateUpdateSystems.Add(lateUpdateSystem);
+			}
+
+			if (system is IDestroySystem destroySystem)
+			{
+				DestroySystems.Add(destroySystem);
+			}
+
+			return this;
+		}
+
+		public virtual void Init()
+		{
+			for (var i = 0; i < InitSystems.Count; i++)
+			{
+				InitSystems[i].Init();
+			}
+		}
+		
+		public virtual void Update()
+		{
+			for (var i = 0; i < UpdateSystem.Count; i++)
+			{
+				UpdateSystem[i].Update();
+			}
+		}
+		
+		public virtual void FixedUpdate()
+		{
+			for (var i = 0; i < FixedUpdateSystems.Count; i++)
+			{
+				FixedUpdateSystems[i].FixedUpdate();
+			}
+		}
+		
+		public virtual void LateUpdate()
+		{
+			for (var i = 0; i < LateUpdateSystems.Count; i++)
+			{
+				LateUpdateSystems[i].LateUpdate();
+			}
+		}
+
+		public virtual void Destroy()
+		{
+			for (var i = 0; i < DestroySystems.Count; i++)
+			{
+				DestroySystems[i].Destroy();
+			}
+		}
+	}
+}
