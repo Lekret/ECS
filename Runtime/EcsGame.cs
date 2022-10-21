efe0a3d3b06ff2741a30131274ccbd34b67a15a9
Lekret <lekret399@mail.ru>
2022-10-21 19:54:17 +0300

    EcsGame class

---
+++ Runtime/EcsGame.cs
@@ -0,0 +1,20 @@
+﻿namespace Lekret.Ecs
+{
+    public class EcsGame
+    {
+        public EcsGame(EcsSystems systems, EcsManager manager)
+        {
+            Systems = systems;
+            Manager = manager;
+        }
+
+        public EcsSystems Systems { get; }
+        public EcsManager Manager { get; }
+
+        public EcsGame AddSystem(ISystem system)
+        {
+            Systems.Add(system);
+            return this;
+        }
+    }
+}
