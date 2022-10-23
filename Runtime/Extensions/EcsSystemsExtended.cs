66550be88610729c6ca20f23bfcba0ec2e8ea61e
Lekret <lekret399@mail.ru>
2022-10-23 18:08:25 +0300

    EcsSystemsExtended

---
+++ Runtime/Extensions/EcsSystemsExtended.cs
@@ -0,0 +1,45 @@
+﻿using Lekret.Ecs;
+using Lekret.Ecs.Extensions;
+
+namespace SimpleEcs.Runtime
+{
+    public class EcsSystemsExtended : EcsSystems
+    {
+        private readonly EcsManager _manager;
+
+        public EcsSystemsExtended(EcsManager manager)
+        {
+            _manager = manager;
+        }
+
+        public EcsSystemsExtended NotifyOnSet<T>()
+        {
+            Add(new SetSelfEventSystem<T>(_manager));
+            return this;
+        }
+
+        public EcsSystemsExtended NotifyAllOnSet<T>()
+        {
+            Add(new SetAllEventSystem<T>(_manager));
+            return this;
+        }
+        
+        public EcsSystemsExtended NotifyOnRemove<T>()
+        {
+            Add(new RemoveSelfEventSystem<T>(_manager));
+            return this;
+        }
+        
+        public EcsSystemsExtended NotifyAllOnRemove<T>()
+        {
+            Add(new RemoveAllEventSystem<T>(_manager));
+            return this;
+        }
+
+        public EcsSystemsExtended Remove<T>()
+        {
+            Add(new RemoveComponentSystem<T>(_manager));
+            return this;
+        }
+    }
+}
