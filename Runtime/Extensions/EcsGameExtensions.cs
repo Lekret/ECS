633ac8b8fc2c292e9f679e8c8ec8f94ea401b5b1
Lekret <lekret399@mail.ru>
2022-10-23 07:14:10 +0300

    Naming

--- Runtime/Extensions/EcsGameExtensions.cs
+++ Runtime/Extensions/EcsGameExtensions.cs
@@ -2,22 +2,22 @@
 {
     public static class EcsGameExtensions
     {
-        public static EcsGame NotifySet<T>(this EcsGame game)
+        public static EcsGame NotifyOnSet<T>(this EcsGame game)
         {
             return game.AddSystem(new SetSelfEventSystem<T>(game.Manager));
         }
 
-        public static EcsGame NotifySetAll<T>(this EcsGame game)
+        public static EcsGame NotifyAllOnSet<T>(this EcsGame game)
         {
             return game.AddSystem(new SetAllEventSystem<T>(game.Manager));
         }
         
-        public static EcsGame NotifyRemove<T>(this EcsGame game)
+        public static EcsGame NotifyOnRemove<T>(this EcsGame game)
         {
             return game.AddSystem(new RemoveSelfEventSystem<T>(game.Manager));
         }
         
-        public static EcsGame NotifyRemoveAll<T>(this EcsGame game)
+        public static EcsGame NotifyAllOnRemove<T>(this EcsGame game)
         {
             return game.AddSystem(new RemoveAllEventSystem<T>(game.Manager));
         }
